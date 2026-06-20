9ed4687ed515fcbf6b17aec285eb52463c23d37b
Peter Olusesan <olusesanpeter@gmail.com>
2026-06-20 08:55:35 +0100

    Initial Commit

---
+++ Control C/Control_CApp.swift
@@ -0,0 +1,17 @@
+//
+//  Control_CApp.swift
+//  Control C
+//
+//  Created by Peter Olusesan on 20/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Control_CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
